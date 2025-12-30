99573e07b88d235766b14f2b35775224901f13cb
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2025-12-29 22:09:52 -0300

    toy model to plot pt_spectra for pions.

---
+++ spectra_plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Toy model for heavy-ion pT spectra
+pT = np.linspace(.2, 5, 30)
+spectra = np.exp(-pT/.6)
+
+plt.figure(figsize=(6,4))
+plt.semilogy(pT, spectra, 'o-', label=r'$\pi^+$, 0-5%')
+
+plt.xlabel(r'$p_T$ [GeV]')
+plt.ylabel(r'$\frac{1}{2\pi p_T}\frac{d^{2N}}{dp_T dy}$')
+plt.legend()
+plt.grid(alpha=.3)
+
+plt.tight_layout()
+plt.show()
