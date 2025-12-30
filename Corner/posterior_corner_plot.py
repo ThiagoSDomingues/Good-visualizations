bc9b5f82c59f4936edf3db32535cb2b0c53f8e95
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2025-12-30 16:05:37 -0300

    Fix covariance matrix in posterior samples generation

--- Corner/posterior_corner_plot.py
+++ Corner/posterior_corner_plot.py
@@ -4,7 +4,7 @@ import corner
 # Fake data: posterior samples (5,000) from a 2D Gaussian distribution
 samples = np.random.multivariate_normal(
     mean=[0.15, 0.30],
-    cov=[[0.01],[0.005],[0.005, 0.02]],
+    cov=[[0.01,0.005],[0.005, 0.02]],
     size=5000
 )
 
