deaa56050d0710630400f82afb533d7f0365f6df
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2025-12-29 22:14:28 -0300

    Fix ylabel formatting in spectra_plot.py

--- spectra_plot.py
+++ spectra_plot.py
@@ -9,7 +9,7 @@ plt.figure(figsize=(6,4))
 plt.semilogy(pT, spectra, 'o-', label=r'$\pi^+$, 0-5%')
 
 plt.xlabel(r'$p_T$ [GeV]')
-plt.ylabel(r'$\frac{1}{2\pi p_T}\frac{d^{2N}}{dp_T dy}$')
+plt.ylabel(r'$\frac{1}{2\pi p_T}\frac{d^2N}}{dp_T dy}$')
 plt.legend()
 plt.grid(alpha=.3)
 
